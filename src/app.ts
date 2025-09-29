269c4bb99e646532a06c23b77c768266d79d8892
Zakaria Sumon <zsumonn@gmail.com>
2025-09-29 07:48:04 +0600

    Add tsconfig-paths for runtime preload

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:3002",
-  "https://www.spainwin99.com",
+  "https://www.capitalisegfx.com",
   "https://spainwin99.vercel.app",
 ];
 
